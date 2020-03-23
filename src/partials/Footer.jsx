1ec07a48377db1fd66c6cd92509c7d5f2ff6b3f6
muhafaa <muhafa99@gmail.com>
2020-03-23 17:23:44 +0700

    initial commit react js with store and router
    component:
    nambahin homepage

---
+++ src/partials/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <div
+      className="footer py-3 m-0"
+      style={{
+        position: 'relative',
+        backgroundColor: '#2C4054',
+        zIndex: '2',
+        bottom: '0',
+        width: '100%'
+      }}
+    >
+      <Container className="d-flex justify-content-center">
+        <small className="text-center text-white">
+          Copyright © 2020 Dokuin.js Team
+        </small>
+      </Container>
+    </div>
+  )
+}
+
+export default Footer
