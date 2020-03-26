7ed33135e23d335badedadd1adf4312fc7f00017
muhafaa <muhafa99@gmail.com>
2020-03-26 13:23:48 +0700

    login belum

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -66,7 +66,7 @@ export default function() {
     try {
       const response = await signIn({
         variables: {
-          email: email,
+          userIdentifier: email,
           password: password
         }
       })
